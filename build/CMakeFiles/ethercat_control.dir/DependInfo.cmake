4e3fe245b28aed6b5ecd9d86c83226389b20afd3
GuoROS <691452980@qq.com>
2025-07-06 10:28:08 +0000

    20250706

--- build/CMakeFiles/ethercat_control.dir/DependInfo.cmake
+++ build/CMakeFiles/ethercat_control.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/gbb/igh_led/src/main.c" "CMakeFiles/ethercat_control.dir/src/main.c.o" "gcc" "CMakeFiles/ethercat_control.dir/src/main.c.o.d"
+  "/home/gbb/igh_led/src/main.cpp" "CMakeFiles/ethercat_control.dir/src/main.cpp.o" "gcc" "CMakeFiles/ethercat_control.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/ethercat_control.dir/cmake_clean.cmake
+++ build/CMakeFiles/ethercat_control.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/ethercat_control.dir/src/main.c.o"
-  "CMakeFiles/ethercat_control.dir/src/main.c.o.d"
-  "ethercat_control"
-  "ethercat_control.pdb"
+  "======ethercat_control"
+  "======ethercat_control.pdb"
+  "CMakeFiles/ethercat_control.dir/src/main.cpp.o"
+  "CMakeFiles/ethercat_control.dir/src/main.cpp.o.d"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/ethercat_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
